13b10062f16c4c3c54fdf8df45c5cd6812061ce2
LsanzR <luis.sanzr26@icloud.com>
2025-09-22 18:10:38 +0200

    Create Task.java
    feat(entity): crear entidad Task con validaciones y auditoria.

---
+++ src/main/java/com/lsanz/taskmanager/entity/User.java
@@ -0,0 +1,59 @@
+package com.lsanz.taskmanager.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email
+    @NotBlank
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @NotBlank
+    private String name;
+
+    private Instant createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = Instant.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+}
