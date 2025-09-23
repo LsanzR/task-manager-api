42cf4a9c403cfbd40522d9ad246fbd0ec3abc67c
LsanzR <luis.sanzr26@icloud.com>
2025-09-23 07:58:46 +0200

    Update TaskRepositoryTest.java
    test(repository): añadir busqueda por ID del usuario (findByUserId) al repo TaskRepository

--- src/test/java/com/lsanz/taskmanager/repository/TaskRepositoryTest.java
+++ src/test/java/com/lsanz/taskmanager/repository/TaskRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDate;
 
@@ -87,7 +88,52 @@ public class TaskRepositoryTest {
     }
 
     @Test
-    void findByUserId() {
+    void findByUserId_returnsTasksForGivenUser() {
+        // Arrange
+        User user1 = new User();
+        user1.setName("Luis");
+        user1.setEmail("luis@example.com");
+        user1 = userRepository.save(user1);
+
+        User user2 = new User();
+        user2.setName("Andres");
+        user2.setEmail("Andres@example.com");
+        user2 = userRepository.save(user2);
+
+        Task task1 = new Task();
+        task1.setTitle("Demo task 1");
+        task1.setStatus(TaskStatus.OPEN);
+        task1.setDueDate(LocalDate.of(2025,1,1));
+        task1.setUser(user1);
+        taskRepository.save(task1);
+
+        Task task2 = new Task();
+        task2.setTitle("Demo task 2");
+        task2.setStatus(TaskStatus.OPEN);
+        task2.setDueDate(LocalDate.of(2025,1,1));
+        task2.setUser(user2);
+        taskRepository.save(task2);
+
+        // Act
+        var result = taskRepository.findTasksByUserId(user1.getId(), PageRequest.of(0,10));
+
+        // Assert
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent()
+                .stream()
+                .map(Task::getTitle)
+                .toList())
+                .containsExactlyInAnyOrder("Demo task 1");
+        assertThat(result.getContent().get(0).getUser().getId()).isEqualTo(user1.getId());
+    }
+
+    @Test
+    void findByUserIdAndStatus() {
+
+    }
+
+    @Test
+    void findByDueDateBetween() {
 
     }
 }
